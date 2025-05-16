dd6301abc6c61057ca9351ae35878b84e8207cbd
Basowl <basowlarc@gmail.com>
2025-05-16 13:30:21 +0100

    updated formatting

--- auto_file.py
+++ auto_file.py
@@ -5,27 +5,28 @@
 
 import os
 
+
 def import_file() -> list[str]:
     """Open a given file and return a list of the contents with blank lines removed"""
     imported_list = []
 
-    with open("names.txt", "r") as file: # can change this to be a variable given to the function to define other files.
+    with open("names.txt", "r") as file:
         for line in file:
-            if line.strip() != "": # checks for blank entries and excludes them.
+            if line.strip() != "":  # checks for blank entries and excludes them.
                 imported_list.append(line.strip())
-    
+
     return imported_list
 
 
 def create_parent_dir() -> None:
     """Creates a directory for the ease of file management post creation, can also be used to create multiple"""
-    PATH = "names" # this can be changed to make the function take an argument to create multiple dirs if needed. 
+    PATH = "names"  # this can be changed to make the function take an argument to create multiple dirs if needed.
 
-    if not os.path.exists(PATH): # if the given dir name doesn't exist the next line creates it.
+    if not os.path.exists(PATH):  # if the given dir doesn't exist.
         os.makedirs(PATH)
 
 
-def create_dir(import_file :list[str], parent_dir :str) -> None:
+def create_dir(import_file: list[str], parent_dir: str) -> None:
     """creates a directory for each line of import_file in the directory given in parent_dir"""
     for line in import_file:
 
@@ -33,27 +34,28 @@ def create_dir(import_file :list[str], parent_dir :str) -> None:
             os.makedirs("names/" + line, exist_ok=True)
 
 
-def create_file(import_file :list[str]) -> None:
+def create_file(import_file: list[str]) -> None:
     """Creates a file with the name on each line of the given list, does not return anything"""
-    for line in import_file: # repeats for each name in imported_list
+    for line in import_file:  # repeats for each name in imported_list
 
-        file_path = os.path.join("names", line) # this returns a file path using the correct seperator. Makes this work on windows AND linux.
-        open(file_path, "x") # this creates a file in the folder above.
+        file_path = os.path.join(
+            "names", line
+        )  # returns a file path using the correct seperator.
+        open(file_path, "x")  # creates a file with the line name..
 
 
-def user_choice(parent_dir :str) -> None:
+def user_choice(parent_dir: str) -> None:
     """gets user input to determine the creation of directories or files."""
     choice = input("1 for Directories only, 2 for files only: ").strip().lower()
 
     if choice == "1":
-        create_dir(import_file() , parent_dir)        
+        create_dir(import_file(), parent_dir)
 
     elif choice == "2":
         create_file(import_file())
-    
+
     else:
         print("choice is invalid. 1 or 2 are valid choices.")
-
 
 
 def main() -> None:
@@ -61,9 +63,9 @@ def main() -> None:
 
     create_parent_dir()
     user_choice("names")
-    
 
     print("Main has finished running.")
+
 
 if __name__ == "__main__":
     main()
